e04a389b665ea80ecdb82208bb72abbd9424192e
DaScientist <37672507+DaScientist@users.noreply.github.com>
2020-04-11 19:22:16 +0530

    Changed index file extension

--- index.php
+++ index.php
@@ -27,34 +27,34 @@
             </div>
         </div>
         <div class="main">
-            <!-- <div class="carousel-container-custom container"> -->
-                <!-- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel"> -->
-                    <!-- <ol class="carousel-indicators"> -->
-                        <!-- <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li> -->
-                        <!-- <li data-target="#carouselExampleIndicators" data-slide-to="1"></li> -->
-                        <!-- <li data-target="#carouselExampleIndicators" data-slide-to="2"></li> -->
-                    <!-- </ol> -->
-                    <!-- <div class="carousel-inner"> -->
-                        <!-- <div class="carousel-item active"> -->
-                            <!-- <img src="assets/422222.jpg" class="d-block w-100" alt="..."> -->
-                        <!-- </div> -->
-                        <!-- <div class="carousel-item"> -->
-                            <!-- <img src="assets/677515.jpg" class="d-block w-100" alt="..."> -->
-                        <!-- </div> -->
-                        <!-- <div class="carousel-item"> -->
-                            <!-- <img src="assets/bg-count-downs.jpg" class="d-block w-100" alt="..."> -->
-                        <!-- </div> -->
-                    <!-- </div> -->
-                    <!-- <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev"> -->
-                        <!-- <span class="carousel-control-prev-icon" aria-hidden="true"></span> -->
-                        <!-- <span class="sr-only">Previous</span> -->
-                    <!-- </a> -->
-                    <!-- <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next"> -->
-                        <!-- <span class="carousel-control-next-icon" aria-hidden="true"></span> -->
-                        <!-- <span class="sr-only">Next</span> -->
-                    <!-- </a> -->
-                <!-- </div> -->
-            <!-- </div> -->
+            <div class="carousel-container-custom container"> 
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="assets/422222.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="assets/677515.jpg" class="d-block w-100" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="assets/bg-count-downs.jpg" class="d-block w-100" alt="...">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
         </div>
         <div class="footer">This is the footer</div>
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
