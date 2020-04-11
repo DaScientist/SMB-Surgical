40a66472b8346bcb776b78c2a4ba5ff958185ff8
DaScientist <37672507+DaScientist@users.noreply.github.com>
2020-04-12 00:52:59 +0530

    Little styling and setting up the navHeader

--- index.php
+++ index.php
@@ -1,71 +1,72 @@
 <!DOCTYPE html>
 <html lang="en">
 
-    <head>
-        <meta charset="UTF-8">
-        <title>SMB Surgicals</title>
-        <link rel="stylesheet" href="style/stylesheet.css">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-            integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    </head>
+<head>
+    <meta charset="UTF-8">
+    <title>SMB Surgicals</title>
+    <link rel="stylesheet" href="style/stylesheet.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
 
-    <body id="body">
-        <div class="header">
-            <a href="#content" class="company-logo">
-                <div>
-                    <img src="./assets/SMB_edited.png" />
-                </div>
+<body id="body" style="background-color:aqua">
+    <div class="header">
+        <a href="./index.php" class="company-logo">
+            <div>
+                <img src="./assets/SMB_edited.png" />
+            </div>
+        </a>
+        <nav class="header-buttons">
+            <a href="./index.php" id="home">
+                <div>Home</div>
             </a>
-            <div class="header-buttons">
-                <ul style="display: flex; align-items: center;">
-                    <li style="display:block"><a href="#body" id="home">Home</a></li>
-                    <li style="display:inline-block"><a href="pages/Products.html" id="Products">Products</a></li>
-                    <li style="display:inline-block"><a href="pages/Aboutus.html" id="About">About Us</a></li>
-                    <li style="display:inline-block"><a href="pages/contactus.html" id="Contact-us">Contact Us</a></li>
-                    <li style="display:inline-block"><a href="pages/Login.php" id="Login">Login</a></li>
-                </ul>
-            </div>
-        </div>
-        <div class="main">
-            <div class="carousel-container-custom container"> 
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="assets/422222.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/677515.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="assets/bg-count-downs.jpg" class="d-block w-100" alt="...">
-                        </div>
+            <a href="pages/Products.html" id="Products">
+                <div>Products</div>
+            </a>
+            <a href="pages/Aboutus.html" id="About">
+                <div>About Us</div>
+            </a>
+            <a href="pages/contactus.html" id="Contact-us">
+                <div>Contact Us</div>
+            </a>
+            <a href="pages/Login.php" id="Login">
+                <div>Login</div>
+            </a>
+        </nav>
+    </div>
+    <div class="main">
+        <div class="carousel-container-custom container">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="assets/422222.jpg" class="d-block w-100" alt="...">
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+                    <div class="carousel-item">
+                        <img src="assets/677515.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="assets/bg-count-downs.jpg" class="d-block w-100" alt="...">
+                    </div>
                 </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
         </div>
-        <div class="footer">This is the footer</div>
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-            integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-            crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-            crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-            integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-            crossorigin="anonymous"></script>
-    </body>
+    </div>
+    <div class="footer">This is the footer</div>
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
 
 </html>
