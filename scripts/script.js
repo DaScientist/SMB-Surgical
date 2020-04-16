da13f46ce95f7ecb6e9dcb349d56ae0970c8c5af
DaScientist <37672507+DaScientist@users.noreply.github.com>
2020-04-16 23:27:36 +0530

    Added footer

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-function dropNav() {
-  var x = document.getElementById("topNav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
